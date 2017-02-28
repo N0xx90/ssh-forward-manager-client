a15d54f3c8415ebaad98ee895ca2d7ba01924482
N0xx <jdc.tiago@gmail.com>
2017-02-28 17:09:46 +0100

    fix systemd script location

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=['pyaml'],
     packages=['sfmclient'],
     data_files=[('/etc/sfmclient/', ['conf/config.yml']),
-                ('/etc/systemd/user/',['conf/sfmclient.service'])],
+                ('/etc/systemd/system/',['conf/sfmclient.service'])],
     entry_points = {
         'console_scripts': [
             'sfmclient = sfmclient.main:main',
